6f179afaa2996ec4ba4000f44981fa70e9565025
jurineide <jurineidesouza@gmail.com>
2023-08-21 09:57:31 -0300

    operadores

---
+++ src/Operadores.java
@@ -0,0 +1,19 @@
+// operador de atribuição , aplica uma atribuição a uma variavel =
+// operadores aritmeticos, adição(+), subtração (-), multiplicação(*), divisão (/),resto ou também conhecido como modulo (%).
+// operador + se usado em stryng ele será usado como concatenação
+// a partir do momento que o depurador identifica a concatenação, o restante ele concatena idependente de ser valor numerico. Com excessão se tiver em parenteses.
+
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto= "Jurineide " + "Souza";
+
+        System.out.println(nomeCompleto);
+
+        String valor= "1"+1+1+1;
+
+        System.out.println(valor);
+
+
+    }
+}
