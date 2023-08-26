fafd3328db9fee2914b6addec2862d2de1ab6a2c
jurineide <jurineidesouza@gmail.com>
2023-08-26 08:36:30 -0300

    relacionais

---
+++ src/Relacionais.java
@@ -0,0 +1,13 @@
+public class Relacionais {
+    public static void main(String[] args) {
+        
+        int  numero1=3;
+        int numero2=4;
+        boolean simNao= numero1==numero2;
+
+        System.out.println("numero1 é igual ao numero2 "+ simNao);
+
+
+
+    }
+}
